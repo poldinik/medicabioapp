cef9eb5a2c1e1a2b6ba222284945f76313d49029
poldinik <lorenzo.vannucchi1993@gmail.com>
2018-10-26 19:57:35 +0200

    aggiunge models e viewmodels

--- Medicabio/Views/LectionsPage.xaml.cs
+++ Medicabio/Views/LectionsPage.xaml.cs
@@ -3,15 +3,20 @@ using System.Collections.Generic;
 
 using Xamarin.Forms.Xaml;
 using Xamarin.Forms;
+using Medicabio.ViewModels;
 
 namespace Medicabio.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class LectionsPage : ContentPage
     {
+        LectionsViewModel viewModel;
+
         public LectionsPage()
         {
             InitializeComponent();
+
+            BindingContext = viewModel = new LectionsViewModel();
         }
 
 
