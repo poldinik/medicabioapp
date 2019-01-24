3a0240c1c233586da1bc6f2663ef26e95da9686b
poldinik <lorenzo.vannucchi1993@gmail.com>
2019-01-24 18:09:30 +0100

    Modifica productviewmodel

--- Medicabio/ViewModels/ProductViewModel.cs
+++ Medicabio/ViewModels/ProductViewModel.cs
@@ -1,10 +1,68 @@
 ﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using Medicabio.Models;
+using Medicabio.Services;
+
 namespace Medicabio.ViewModels
 {
-    public class ProductViewModel
+    public class ProductViewModel : INotifyPropertyChanged
     {
-        public ProductViewModel()
+
+        public RestService restService { get; set; }
+
+        private Product product;
+
+        public Product Product
         {
+            get
+            {
+                return product;
+            }
+            set
+            {
+                product = value;
+                OnPropertyChanged();
+            }
         }
+
+
+        public ProductViewModel(int Id)
+        {
+            restService = new RestService();
+            LoadProduct(Id);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            Debug.WriteLine("property name: " + propertyName);
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+        public async void LoadProduct(int Id)
+        {
+            //List<Key> Keys = await GetKeysAsync();
+
+            Product = await GetProductAsync(Id);
+            Console.WriteLine(Product);
+            //OKeys = new ObservableCollection<Key>(Keys as List<Key>);
+            //Console.WriteLine(_okeys[1].id);
+        }
+
+      
+        public Task<Product> GetProductAsync(int Id)
+        {
+            //return restService.GetResponse...
+            Debug.WriteLine("get Product " + Id.ToString());
+            return restService.GetResponse<Product>(Constants.urlApi + "products/"+Id.ToString(), false);
+        }
+
+
+
     }
 }
